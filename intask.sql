a42d8de328703f21bf66919d5fa37e56e9047cc7
VINICIUS LOPES DA SILVA <0200832311013>
2024-10-25 12:49:30 -0300

    add bg

--- intask.sql
+++ intask.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25-Out-2024 às 17:21
+-- Tempo de geração: 25-Out-2024 às 17:48
 -- Versão do servidor: 10.4.32-MariaDB
 -- versão do PHP: 8.2.12
 
@@ -54,7 +54,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `is_admin`, `role`) VALUES
-(2, 'rafa', '$2y$10$LkfLVrJE64f3zW7bKtyVe.90nwEo/VO8Tbs9fXWshGX.Rpr79HlCS', '2024-10-25 15:07:20', 0, 'user');
+(2, 'rafa', '$2y$10$LkfLVrJE64f3zW7bKtyVe.90nwEo/VO8Tbs9fXWshGX.Rpr79HlCS', '2024-10-25 15:07:20', 0, 'user'),
+(4, 'vnlopes', 'admin', '2024-10-25 15:24:56', 0, 'admin'),
+(5, 'fafa', '$2y$10$e5oSTr76TaJ1dFmxIoGk5OrX/kXGwcvVVq41VHLqbCIT4les0ea96', '2024-10-25 15:38:47', 0, 'user'),
+(6, 'dwada', '$2y$10$pyVX9O919.ko.fj21odrNOCevN5nika0gt5cW.G2KCTpNbdinhSzC', '2024-10-25 15:46:16', 0, 'user'),
+(7, 'dwada123123', '$2y$10$sMBMt1lbbU4WKeNnT87hueUIsBDXSKnpPfoZyX4P9aUPK2cH/KTpm', '2024-10-25 15:47:07', 0, 'user');
 
 --
 -- Índices para tabelas despejadas
@@ -87,7 +91,7 @@ ALTER TABLE `notas`
 -- AUTO_INCREMENT de tabela `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restrições para despejos de tabelas
