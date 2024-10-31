95dd5d7772a6d53906d3ec0cd615b55496efe537
VINICIUS LOPES DA SILVA <0200832311013>
2024-10-31 12:17:33 -0300

    edit

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost"; // Geralmente é 'localhost'
-$username = "root"; // Nome de usuário do banco de dados
-$password = ""; // Senha do banco de dados
-$dbname = "inTask"; // Substitua pelo nome do seu banco de dados
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "inTask";
 
 // Cria a conexão
 $conn = new mysqli($servername, $username, $password, $dbname);
