c80b4893067eae2c3fefacad75836197853997a0
VINICIUS LOPES DA SILVA <0200832311013>
2024-11-01 12:54:09 -0300

    add codigo bd

--- intask.sql
+++ intask.sql
@@ -1,44 +1,24 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 25-Out-2024 às 17:48
--- Versão do servidor: 10.4.32-MariaDB
--- versão do PHP: 8.2.12
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Banco de dados: `intask`
---
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `notas`
---
-
-CREATE TABLE `notas` (
+CREATE TABLE `notes` (
   `id` int(11) NOT NULL,
-  `usuario_id` int(11) NOT NULL,
-  `conteudo` text NOT NULL,
-  `data_criacao` timestamp NOT NULL DEFAULT current_timestamp()
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `users`
---
+INSERT INTO `notes` (`id`, `user_id`, `title`, `content`, `date`) VALUES
+(6, 10, '321312', '3123', '2024-11-01 15:43:58'),
+(7, 10, 'rafa', 'dawdw', '2024-11-01 15:48:49');
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -49,59 +29,30 @@ CREATE TABLE `users` (
   `role` enum('admin','user') DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Extraindo dados da tabela `users`
---
-
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`, `is_admin`, `role`) VALUES
-(2, 'rafa', '$2y$10$LkfLVrJE64f3zW7bKtyVe.90nwEo/VO8Tbs9fXWshGX.Rpr79HlCS', '2024-10-25 15:07:20', 0, 'user'),
 (4, 'vnlopes', 'admin', '2024-10-25 15:24:56', 0, 'admin'),
-(5, 'fafa', '$2y$10$e5oSTr76TaJ1dFmxIoGk5OrX/kXGwcvVVq41VHLqbCIT4les0ea96', '2024-10-25 15:38:47', 0, 'user'),
-(6, 'dwada', '$2y$10$pyVX9O919.ko.fj21odrNOCevN5nika0gt5cW.G2KCTpNbdinhSzC', '2024-10-25 15:46:16', 0, 'user'),
-(7, 'dwada123123', '$2y$10$sMBMt1lbbU4WKeNnT87hueUIsBDXSKnpPfoZyX4P9aUPK2cH/KTpm', '2024-10-25 15:47:07', 0, 'user');
+(10, 'vnlopes1', '$2y$10$LV9tf0aXxgW8yYAMYEWYd.8fJrg1evgimG20WHWNKdq9/Qu1WFhze', '2024-11-01 13:01:56', 0, 'user'),
+(11, 'rafa', '$2y$10$UKknhiovCS/wYt8vEH0JSuqh8aeXc7OBQqKZyOjfN6g99jpOhp2UC', '2024-11-01 13:02:43', 0, 'user'),
+(12, 'vania', '$2y$10$8fpPWyyKEnPLdndYQAYe2OFPgbty5usOK62hCSulPsQtzDzt8q7De', '2024-11-01 14:10:59', 0, 'user');
 
---
--- Índices para tabelas despejadas
---
 
---
--- Índices para tabela `notas`
---
-ALTER TABLE `notas`
+ALTER TABLE `notes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `usuario_id` (`usuario_id`);
+  ADD KEY `idx_user_id` (`user_id`);
 
---
--- Índices para tabela `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT de tabelas despejadas
---
 
---
--- AUTO_INCREMENT de tabela `notas`
---
-ALTER TABLE `notas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
+ALTER TABLE `notes`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
---
--- Restrições para despejos de tabelas
---
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- Limitadores para a tabela `notas`
---
-ALTER TABLE `notas`
-  ADD CONSTRAINT `notas_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `users` (`id`);
+
+ALTER TABLE `notes`
+  ADD CONSTRAINT `fk_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
